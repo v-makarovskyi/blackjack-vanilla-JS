7cf11b6b06c77329be9469f769fc4befc402eb59
Vladimir <bigcorvet@gmail.com>
2023-06-18 13:50:00 +0300

    added shuffle

--- index.js
+++ index.js
@@ -20,3 +20,14 @@ const createDeck = () => {
     }
     return deck
 }
+
+const shuffle =() => {
+    for(let i = 0; i < 1000; i++) {
+        let location1 = Math.floor((Math.random() * deck.length))
+        let location2 = Math.floor((Math.random() * deck.length))
+        let tmp = deck[location1]
+
+        deck[location1] = deck[location2]
+        deck[location2] = tmp
+    }
+}

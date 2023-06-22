0dbc5b75047e176e8c875ddb432418aefd376dbf
Vladimir <bigcorvet@gmail.com>
2023-06-22 12:14:06 +0300

    create func createDeck()

--- index.js
+++ index.js
@@ -4,127 +4,17 @@ const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Валет', 'Да
 const deck = new Array()
 const players = new Array()
 
-const createDeck = () => {
+//Создаем колоду карт для игры
+function createDeck() {
     for(let c = 0; c < cards.length; c++) {
         for(let s = 0; s < suits.length; s++) {
             let weight = parseInt(cards[c])
-            if(cards[c] === 'Валет' || cards[c] === 'Дама' || cards[c] === 'Король') {
-                weight = 10
-            }
-            if(cards[c] === 'Туз') {
-                weight = 11
-            } 
-            const card = {Value: cards[c], Suit: suits[s], Weight:weight}
+            if(cards[c] === 'Валет' || cards[c] === 'Дама' || cards[c] === 'Король') weight = 10
+            if(cards[c] === 'Туз') weight = 11
+            const card = { Value: cards[c], Suit: suits[s], Weight: weight }
             deck.push(card)
         }
     }
     return deck
 }
-
-const shuffle =() => {
-    for(let i = 0; i < 1000; i++) {
-        let location1 = Math.floor((Math.random() * deck.length))
-        let location2 = Math.floor((Math.random() * deck.length))
-        let tmp = deck[location1]
-
-        deck[location1] = deck[location2]
-        deck[location2] = tmp
-    }
-}
-
-
-const createPlayers = (num) => {
-    for(let i = 1; i < num; i++) {
-        const hand = new Array()
-        const player = {Name:'Player', ID: i, Points: 0, Hand: hand}
-        players.push(player)
-    }
-    
-}
-createPlayers(2)
-
-
-//создание пользовательского интерфейса 
-const createPlayersUI = () => {
-    document.getElementById('players').innerHTML = ''
-
-    for(let i = 0; i < players.length; i++) {
-        const div_player = document.createElement('div')
-        const div_playerid = document.createElement('div')
-        const div_hand = document.createElement('div')
-        const div_points = document.createElement('div')
-
-        div_points.className = 'points'
-        div_points.id = 'points_' + i
-        div_player.id = 'player_' + i
-        div_player.className = 'player'
-        div_hand.id = 'hand_' + i
-
-        div_playerid.innerHTML = 'Player ' + players[i].ID
-        div_player.appendChild(div_playerid)
-        div_player.appendChild(div_hand)
-        div_player.appendChild(div_points)
-
-        document.getElementById('players').appendChild(div_player)
-        
-    }
-}
-createPlayersUI()
-
-//поочередная раздача карт каждому игроку
-//по 2 карты каждому
-const dealHands = () => {
-    for(let i = 0; i < 2; i++) {
-        for(let j = 0; j < players.length; j++) {
-            const card = deck.pop()
-            players[j].Hand.push(card)
-            renderedCard(card, j)
-            updatePoints()
-        }
-    }
-    updateDeck()
-}
-
-const renderedCard = (card, player) => {
-    const hand = document.getElementById('hand_'+player)
-    hand.appendChild(getCardUI(card))
-}
-
-const getCardUI = (card) => {
-    const el = document.createElement('div')
-    let icon = ''
-    if(card.Suit === 'chervy') {
-        icon = '&hearts;'
-    } else if (card.Suit === 'picki') {
-        icon = '&spades;'
-    } else if(card.Suit === 'bubny') {
-        icon = '&diams;'
-    } else {
-        icon = '&clubs;'
-    }
-    el.className = 'card'
-    el.innerHTML = card.Value+ <br/> + icon
-    return el
-}
-
-const getPoints = (player) => {
-    let points = 0
-    for(let i = 0; i < players[player].Hand.length; i++) {
-        points += players[player].Hand[i].weight
-    } 
-    players[player].Points = points
-    return points
-}
-
-const updatePoints = () => {
-    for(let i = 0; i < players.length; i++)
-    getPoints(i)
-    document.getElementById('points_' + i).innerHTML = players[i].Points
-}
-
-//Обновление колоды после сдачи карт
-const updateDeck = () => {
-    document.getElementById('deckcount').innerHTML = deck.length
-}
-
 
